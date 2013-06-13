bd1d19daf4fa36598995be19c2ab334661ac5418
Marty Minton <marty.minton@gmail.com>
2013-06-13 09:55:25 -0400

    updated nav

--- inc/topnav.php
+++ inc/topnav.php
@@ -15,18 +15,35 @@
 				      <a class="brand" href="#">Administrator</a>
 				 
 				      <!-- Everything you want hidden at 940px or less, place within here -->
-				      <div class="nav-collapse collapse">
+				      <div class="nav-collapse collapse navbar-inverse-collapse">
 				        <!-- .nav, .navbar-search, .navbar-form, etc -->
-				        <ul class="nav nav-pills">
+				        <ul class="nav">
 					        <li><a href="#">Home</a></li>
-					        <li><a href="logout.php">Logout</a></li>
-					        <li><a href="addUser.php">Add User</a></li>
-					        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Company Profile</a>
+					        <li class="dropdown">
+					        	<a href="#" class="dropdown" data-toggle="dropdown">Manage Users</a>
 					        	<ul class="dropdown-menu">
-						        	<li><a href="#">Login/Logout</a></li>
-						        	<li><a href="#">Login/Logout</a></li>
+						        	<li><a href="addUser.php">Add New User</a></li>
+						        	<li><a href="#">Modify a User</a></li>
 					        	</ul>
 					        </li>
+					        <li class="dropdown">
+					        	<a href="#" class="dropdown" data-toggle="dropdown">Manage Customers</a>
+					        	<ul class="dropdown-menu">
+						        	<li><a href="#">Add New Customer</a></li>
+						        	<li><a href="#">Modify a Customer</a></li>
+					        	</ul>
+					        </li>
+					        <li><a href="#">Timesheets</a></li>
+					        <li class="dropdown">
+					        	<a href="#" class="dropdown" data-toggle="dropdown">Reports</a>
+					        	<ul class="dropdown-menu">
+						        	<li><a href="#">Project Report</a></li>
+						        	<li><a href="#">Employee Hour Report</a></li>
+						        	<li><a href="#">Customer Report</a></li>
+						        	<li><a href="#">Profit vs Loss Report</a></li>
+					        	</ul>
+					        </li>
+					        <li><a href="logout.php">Logout</a></li>
 				        </ul>
 				      </div>
 				 
