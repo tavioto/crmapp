f2ec7bbb331b185b35ed43ae2909cc9012c7de65
Jorge Ramirez <jarg5487@gmail.com>
2013-06-14 10:33:18 -0500

    Add and edit customer

---
+++ addCustomer.php
@@ -0,0 +1,92 @@
+<?php 
+require_once("includes/includes.php");
+if(!$auth->userLogged()){
+	header('location: index.php');
+}
+
+require_once('inc/header.php'); 
+require_once('inc/topnav.php'); 
+
+extract($_REQUEST);
+$saved = 0;
+
+if(isset($_POST['action']) && $_POST['action'] != ""){
+		
+		extract($_POST);
+		
+		switch($action){
+			case "add":
+				$customer = new Customer();
+				$customer->company_name = $company_name;
+				$customer->phone = $phone;
+				$customer->state_id = $state;
+				$customer->city_id = $city;
+				$customer->email = $email;
+				$customer->company_contact = $company_contact;
+				$customer->address = $address;
+				$customer->zip = $zip;
+				$customer->website = $website;
+				$customer->save();
+	        	$saved = 1;
+	        	
+		break;
+		case "edit":
+				$customer = new Customer($customer_id);
+				$customer->company_name = $company_name;
+				$customer->phone = $phone;
+				$customer->state_id = $state;
+				$customer->city_id = $city;
+				$customer->email = $email;
+				$customer->company_contact = $company_contact;
+				$customer->address = $address;
+				$customer->zip = $zip;
+				$customer->website = $website;
+				$customer->save();
+	        	$saved = 1;
+		break;
+	}
+}
+
+
+if(isset($customer_id)){
+	$customer = new Customer($customer_id);
+	$city_model = new City();
+	$city_ = $city_model->fetchAll(array(array('state_id', '=', $customer->state_id)));
+}
+
+$state_model = new State();
+$state = $state_model->fetchAll();
+
+?>
+<section id="<addcustomer></addcustomer>">
+	<h1><?php if(isset($customer_id)){echo "Edit";}else{echo "Add";}?> a Customer</h1>
+	<?php if($saved == 1){?>
+	<div class="alert alert-success">
+		<strong>Done!</strong> The Customer was <?php if(isset($customer_id)){echo "Edited";}else{echo "Created";}?>.
+	</div>
+	<?php } ?>
+	<form class="add-customer" action="" method="post">
+		<input type="text" class="input-block-level" placeholder="Company Name" name="company_name" id="company_name" value="<?php echo $customer->company_name;?>" required>
+		<input type="text" class="input-block-level" placeholder="Phone" name="phone" id="phone" required value="<?php echo $customer->phone;?>">
+		<select name="state" id="state" class="input-block-level">
+			<option value="">--State--</option>
+			<?php foreach($state as $st){?>
+			<option value="<?php echo $st->id; ?>" <?php if($customer->state_id == $st->id){echo "selected";}?>><?php echo $st->name; ?></option>
+			<?php } ?>
+		</select>
+		<select name="city" id="city" class="input-block-level">
+			<option value="">--City--</option>
+			<?php foreach($city_ as $ct){?>
+			<option value="<?php echo $ct->id; ?>" <?php if($customer->city_id == $ct->id){echo "selected";}?>><?php echo $ct->name; ?></option>
+			<?php } ?>
+		</select>
+		<input type="email" class="input-block-level" placeholder="Email" name="email" id="email" required value="<?php echo $customer->email;?>">
+		<input type="text" class="input-block-level" placeholder="Company Contact" name="company_contact" id="company_contact" required value="<?php echo $customer->company_contact;?>">
+		<input type="text" class="input-block-level" placeholder="Address" name="address" id="address" required value="<?php echo $customer->address;?>">
+		<input type="text" class="input-block-level" placeholder="Zip Code" name="zip" id="zip" required value="<?php echo $customer->zip;?>">
+		<input type="text" class="input-block-level" placeholder="Website URL" name="website" id="website" required value="<?php echo $customer->website;?>">
+		<input type="hidden" class="input-block-level" value="<?php if(!$customer_id){echo "add";}else{echo "edit";}?>" name="action">
+		<button class="btn btn-large btn-primary" type="submit">Save</button>
+	</form>
+</section>
+<?php require_once('inc/footer.php'); ?>
--- userDetail.php
+++ userDetail.php
@@ -8,9 +8,93 @@ if(!$auth->userLogged()){
 require_once('inc/header.php'); 
 require_once('inc/topnav.php'); 
 
+extract($_REQUEST);
+$user = new User($user_id);
+
+$role = new Role($user->id_role);
+$company = new Company($user->id_company);
+$state = new State($user->state_id);
+$city = new City($user->city_id);
+
+if($user->active == 1){
+	$active = 'Active';
+}else{
+	$active = 'Inactive';
+}
+
+
 ?>
 <section id="userDetail">
 	<h1>User Detail</h1>
-	
+	<legend><?php echo $user->first_name." ".$user->last_name; ?></legend>
+          	<table class="table table-hover table-bordered">
+	            <tbody>
+	            	<tr>
+	            		<th>Role Type</th>
+	            		<td><?php echo $role->name; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Name</th>
+	            		<td><?php echo $user->first_name." ".$user->last_name; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Mail</th>
+	            		<td><?php echo $user->mail; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Username</th>
+	            		<td><?php echo $user->username; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Status</th>
+	            		<td><?php echo $active; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Company</th>
+	            		<td><?php echo $company->name; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>State</th>
+	            		<td><?php echo $state->name; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>City</th>
+	            		<td><?php echo $city->name; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Address</th>
+	            		<td><?php echo $user->address; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Zipcode</th>
+	            		<td><?php echo $user->zip; ?></td>
+	            	</tr>
+	            	<tr>
+	            		<th>Phone</th>
+	            		<td><?php echo $user->phone; ?></td>
+	            	</tr> 
+	            	<tr>
+	            		<th>Birthday</th>
+	            		<td><?php echo $user->birthday; ?></td>
+	            	</tr> 
+	            	<tr>
+	            		<th>Employment Start Date</th>
+	            		<td><?php echo $user->emp_start_day; ?></td>
+	            	</tr> 
+	            	<tr>
+	            		<th>Employment Type</th>
+	            		<td><?php echo $user->emp_type; ?></td>
+	            	</tr> 
+	            	<tr>
+	            		<th>Hourly per Rate</th>
+	            		<td><?php echo $user->hourly_pay; ?></td>
+	            	</tr> 
+	            	<tr>
+	            		<th>Hourly Charge Rate</th>
+	            		<td><?php echo $user->hourly_charge; ?></td>
+	            	</tr> 
+	            		
+	            </tbody>
+            </table>
 </section>
 <?php require_once('inc/footer.php'); ?>
